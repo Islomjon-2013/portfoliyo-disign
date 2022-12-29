3d96c2a5ac520112ff7951b7ffcfa42e8af35f57
hulkar <hulkar@gmail.com>
2022-12-28 20:36:47 -0500

    to created portfoliyo

---
+++ main.js
@@ -0,0 +1,26 @@
+// let myBtn = document.querySelector(`.myBtn`);
+// let lights = document.querySelectorAll(`.light`);
+// let darks = document.querySelectorAll(`.dark`)
+// let bi = document.querySelector(`.bi`)
+// myBtn.addEventListener(`click`,function(){
+   
+// for(let light of lights){
+//     light.style.backgroundColor ='white'
+// } 
+// for( black of darks){
+//     black.style.backgroundColor = '#F6F6F6'
+//         }
+//         myBtn.innerHTML=`<i class="bi bi-moon"></i>`
+
+// if(bi.classList.contains( `bi-sun`)){
+//     bi.classList.remove(`bi-sun`)
+// bi.classList.add(`bi-moon`)
+// }else{
+//     bi.classList.remove(`bi-moon`)
+//     bi.classList.add(`bi-sun`)
+// }
+
+
+    
+      
+// });
